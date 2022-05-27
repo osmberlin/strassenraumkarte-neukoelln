70c5f5694f3382a0947e452f6adb467dab686a80
Tobias <tobias@fixmycity.de>
2022-05-27 21:46:52 +0200

    Tailwind config: Listen to `./_sites`
    There was a weird bug where some classes from `posts/index.html` where not recognized. This solves the issue but also makes TW JIT run a lot more stuff. It's still super fast, though.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  content: [
-    "./_includes/**/*.*",
-    "./_layouts/**/*.*",
-    "./_posts/**/*.*",
-    "./javascript/**/*.js",
-    "./parkraumkarte/**/*.js",
-    "./posts/**/*.js",
-    "./parkraumkarte/report.md",
-    "./parkraumkarte/data.html",
-    "./*.md",
-    "./*.html",
-  ],
+  content: ["./_site/**/*.*"],
   theme: {
     extend: {
       zIndex: {
